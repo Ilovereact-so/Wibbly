ea4c78b4f5f91df0949ea3080ce2b4a2bc3f8065
Ilovereact-so <wybranieckamilpl@onet.pl>
2023-12-26 02:15:59 +0100

    #

--- src/components/Profile/MainProfile.js
+++ src/components/Profile/MainProfile.js
@@ -18,6 +18,7 @@ const MainProfile = () => {
     const [H_option, setH_option] = useState({1:'', 2:'',3:'',4:''})
     const [W_option, setW_option] = useState({1:'', 2:'',3:'',4:''})
     const [distance_option, setDistance_option] = useState({1:'', 2:'',3:'',4:''})
+    const [localpallete, setLocalpallete] = useState(JSON.parse(localStorage.getItem('Pallete')))
 
     const navigate = useNavigate()
     function user() {
@@ -28,16 +29,33 @@ const MainProfile = () => {
         }
     }
 
-    const Calcualte_T_option = () => {
+    useEffect(() => {
+
+        const alertMessage = () => {
+          //alert('localStorage changed!');
+          setLocalpallete(JSON.parse(localStorage.getItem('Pallete')))
+          console.log("localStorage changed!'")
+        }
+    
+        //window.localStorage.setItem("item", 'val 1');
+        window.addEventListener('Pallete', alertMessage);
+    
+        //Remove the event listener when the component unmounts
+        return () => {
+          window.removeEventListener("Pallete", alertMessage);
+        }
+      }, []);
+
+    const Calcualte_T_option = async () => {
         setT_option({
             1 : $('#Configure').offset().top,
             2 : $('#Support').offset().top,
             3 : $('#Settings').offset().top,
-            4: $('#Messages').offset().top
+            4 : $('#Messages').offset().top
         })
 
     }
-    const Calcualte_L_option = () => {
+    const Calcualte_L_option = async () => {
         setL_option({
             1 : $('#Configure').offset().left,
             2 : $('#Support').offset().left,
@@ -46,7 +64,7 @@ const MainProfile = () => {
         });
 
     }
-    const Calcualte_H_option = () => {
+    const Calcualte_H_option = async () => {
         setH_option({
             1 : $('#Configure').height(),
             2 : $('#Support').height(),
@@ -55,7 +73,7 @@ const MainProfile = () => {
         });
 
     }
-    const Calcualte_W_option = () => {
+    const Calcualte_W_option = async () => {
         setW_option({
             1 : $('#Configure').width(),
             2 : $('#Support').width(),
@@ -91,20 +109,32 @@ const MainProfile = () => {
         //console.log(themost, themostindex)
     }
 
-     const MenuPos = () => {
-        //Calcualte_T_option()
-        //Calcualte_H_option()
-        Calcualte_L_option()
-        Calcualte_W_option()
-        Calcualte_T_option()
-        //console.log(Calculate_Index())
-    }
+    const MenuPos = async () => {
+        try {
+          // Zamiast bezpośredniego wywołania, użyj await
+          // await Calculate_T_option();
+          // await Calculate_H_option();
+          await Calcualte_L_option();
+          await Calcualte_W_option();
+          await Calcualte_T_option();
+          
+          // Jeśli Calculate_Index również jest asynchroniczne, użyj await
+          // console.log(await Calculate_Index());
+          
+          // Jeśli Calculate_Index nie jest asynchroniczne, można go po prostu wywołać
+          // console.log(Calculate_Index());
+        } catch (error) {
+            
+          // Obsłuż błędy, jeśli istnieją
+        }
+      };
     useEffect(()=>{
-        Reload()
+        //Reload()
+        
     },[])
 
-    const Reload = () =>{
-        MenuPos()
+    const Reload = async () =>{
+        await MenuPos()
         var scroll = $(window).scrollTop();
         var max_scroll = $(window).height()
         const startDeg = 30 //choose strart position
@@ -124,17 +154,20 @@ const MainProfile = () => {
 
   return (
     <div className='flex w-full h-[200vh]'> {/**Background */}
-        <div id='Main' className='flex w-full h-[100vh] fixed'>
-            <div className='flex justify-center w-full float-right h-auto m-[40px] z-[2] bg-[rgba(236,236,236,0.55)] relative rounded-[40px]'>
-            <div onClick={() => navigate("/")} className='absolute top-7 left-7 flex justify-center rounded-[30px] cursor-pointer items-center bg-white'>
-                <img src={Home} className='px-7 py-3 scale-[0.8]'/>
-            </div>
-                <div className='w-[57vw] h-full  flex justify-center items-center mr-[50px]'>
-                    <div className='w-[45vw] h-[45vw] flex justify-center items-center'>
-                        <div id='menu' className='rounded-full relative bg-[rgba(169,169,169,0.16)] backdrop-blur-[10px] w-full flex justify-center items-center h-full'>
+        <div id='Main' className='flex w-full h-[100vh] fixed bg-[#F6F7F8]'>
+            <div className='flex justify-start w-full float-right h-auto m-[40px] z-[2] relative'>
+                <div onClick={() => {navigate("/"); window.location.reload(false);}} style={{backgroundColor: localpallete[0].color}} className='absolute top-7 left-7 flex justify-center rounded-[30px] cursor-pointer items-center'>
+                    <img src={Home} className='px-7 py-3  scale-[0.8]'/>
+                </div>
+                <div onClick={() => navigate("/")} className='absolute bottom-7 left-7 flex justify-center rounded-full cursor-pointer items-center bg-white'>
+                    <p className='text-black text-[17px] font-bold font-Poppins px-20 py-4'>Wyloguj się</p>
+                </div>
+                <div className='w-[57vw] h-full flex justify-center items-center mr-[70px] pl-[100px] bg-[rgba(236,236,236,0.55)] rounded-[40px]'>
+                    <div className='w-[40vw] h-[40vw] flex justify-center items-center'>
+                        <div id='menu' style={{backgroundColor: localpallete[2].color}} className='rounded-full relative bg-[rgba(169,169,169,0.16)] backdrop-blur-[10px] w-full flex justify-center items-center h-full'>
                             <div className='flex flex-col menu_infoText'>
-                                <p className='text-[17px] font-Poppins text-black pb-2'>Wybrano:</p>
-                                <p className='text-[20px] font-Poppins font-bold text-black'>Konfigurator</p>
+                                <p className='text-[15px] font-Poppins text-black pb-2'>Wybrano:</p>
+                                <p className='text-[17px] font-Poppins font-bold text-black'>Konfigurator</p>
                             </div>
                             <div className='absolute w-full h-full'>
                                 <img src={Konfigurator} id='Configure' className='optionA absolute right-[-100px] origin-top top-[50%] translate-y-[-50%]' ></img>
@@ -145,10 +178,10 @@ const MainProfile = () => {
                         </div>    
                     </div>
                 </div> {/**Menu */}
-                <div className='flex flex-col w-auto h-full max-w-[450px] justify-between'>
+                <div className='flex flex-col w-auto h-full max-w-[470px] justify-between'>
                     <div className='mt-[50px]'>
-                        <p className='font-Poppins font-bold text-[49px] text-black mb-3'>Witaj</p>
-                        <p className='font-Poppins text-[25px] text-black mb-2'>{user()?.username}</p>
+                        <p className='font-Poppins font-bold text-[49px] text-white'>Witaj</p>
+                        <p className='font-Poppins text-[25px] text-white mb-2'>{user()?.username}</p>
                         <p className='font-Poppins text-[16px] text-black'>
                             To twój panel użytkownika. Mozesz tu 
                             zmieniać kolory strony, przejść do
@@ -160,7 +193,7 @@ const MainProfile = () => {
             </div>
 
 
-            <div className='absolute right-[-15%] z-[1] h-full w-[100%] '>
+            <div className='absolute right-[-15%] z-[1] h-full w-[100%] opacity-[0.63]'>
                 <Canvas>
                     <Model6_Figurka/>
                 </Canvas>
--- src/components/Profile/SettingsCard.js
+++ src/components/Profile/SettingsCard.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { decodeToken } from 'react-jwt';
 
 const SettingsCard = () => {
+  const [logged, setLogged] = useState(localStorage.getItem('rt'))
+  const [localpallete, setLocalpallete] = useState(JSON.parse(localStorage.getItem('Pallete')))
+  function user() {
+    if(logged != null){
+        const myDecodedToken = decodeToken(logged);
+        //console.log(myDecodedToken)
+        return myDecodedToken
+    }
+}
+  useEffect(() => {
+
+    const alertMessage = () => {
+      //alert('localStorage changed!');
+      setLocalpallete(JSON.parse(localStorage.getItem('Pallete')))
+      console.log("localStorage changed!'")
+    }
+
+    //window.localStorage.setItem("item", 'val 1');
+    window.addEventListener('Pallete', alertMessage);
+
+    //Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("Pallete", alertMessage);
+    }
+  }, []);
+
   return (
-    <div className='w-full h-auto min-h-[450px] py-[30px] px-[40px] rounded-[50px] mb-[50px]'>
+    <div className='w-full h-auto min-h-[520px] p-[60px] rounded-[50px] mb-[50px] bg-white'>
       <div className='flex flex-col justify-between h-full'>
           <div>
-              <p className='font-Poppins text-[40px] font-bold text-white pb-2'>Ustawienia</p>
-              <p className='font-Poppins text-[17px] text-white'>zadbaj o to co chcesz</p>
+              <p className='font-Poppins text-[32px] font-bold mt-1 text-black'>Ustawienia</p>
+              <p style={{color: localpallete[3].color}} className='font-Poppins text-[17px]'>zadbaj o to co chcesz</p>
+              <div className='mt-[40px]'>
+                <p className='font-Poppins text-[17px] text-black font-bold'>Zmień <span className='font-[400]'>email</span></p>
+                <input placeholder={user()?.email}  autoCapitalize='off' type='email' autoCorrect='off' className='w-full h-[50px] bg-[#E1E1E1] mt-2 font-Poppins rounded-full text-center'/>
+              </div>
+              <div className='mt-4'>
+                <p className='font-Poppins text-[17px] text-black font-bold'>Zmień <span className='font-[400]'>nazwę urzytkownika</span></p>
+                <input placeholder={user()?.username}  autoCapitalize='off' type="text" autoCorrect='off' className='w-full bg-[#E1E1E1] h-[50px] mt-2 font-Poppins rounded-full text-center'/>
+              </div>
+              
+          </div>
+          <div className='flex  justify-center'>
+                <div style={{backgroundColor: localpallete[1].color}} className='font-Poppins text-white p-4 w-[150px] rounded-full text-center font-bold text-[17px]'>Zapisz</div>
           </div>
           
       </div>
